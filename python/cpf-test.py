94836bd2f09dc44b59ec7fc90790a7b4cd442bcc
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-14 12:10:39 -0300

    Alterado arquivo 'python/cpf-test.py': alterado nome da class de 'TestId' para 'TestCpf', e os nomes dos testes agora iniciam com 'test_cpf' ao invés de 'test_id'.

--- python/cpf-test.py
+++ python/cpf-test.py
@@ -1,7 +1,7 @@
 import pytest
 import cpf
 
-class TestId:
+class TestCpf:
     @pytest.fixture
     def C(self):
         return cpf.Cpf
@@ -11,26 +11,26 @@ class TestId:
         ("11144477735", "111.444.777-35"),
         ("111444777999", False)
     ])
-    def test_id_marcar(self, cpf, resultado, C):
+    def test_cpf_marcar(self, cpf, resultado, C):
         assert C.marcar(cpf) == resultado
     
     @pytest.mark.parametrize("digitos_cpf, resultado", [
         ([1, 1, 1, 4, 4, 4, 7, 7, 7], 3),
         ([1, 1, 1, 4, 4, 4, 7, 7, 7, 3], 5)
     ])
-    def test_id_calcular_dv(self, digitos_cpf, resultado, C):
+    def test_cpf_calcular_dv(self, digitos_cpf, resultado, C):
         assert C.calcular_dv(digitos_cpf) == resultado
     
     @pytest.mark.parametrize("cpf, resultado", [
         (11144477735, (3, 5)),
         (11144477799, (3, 5)),
     ])
-    def test_id_obter_dvs(self, cpf, resultado, C):
+    def test_cpf_obter_dvs(self, cpf, resultado, C):
         assert C.obter_dvs(cpf) == resultado
     
     @pytest.mark.parametrize("cpf, resultado", [
         (11144477735, True),
         (11144477799, False)
     ])
-    def test_id_verificar(self, cpf, resultado, C):
+    def test_cpf_verificar(self, cpf, resultado, C):
         assert C.verificar(cpf) == resultado
