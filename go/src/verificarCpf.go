48f6766478ffb5432b638566cbc3572e621d9e29
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-16 14:02:38 -0300

    Alterado arquivo 'src/verificarCpf.go': 1): a função 'VerificarCpf' está disponível a qualquer instância da struct 'Cpf'. 2): adaptada chamada da função 'CalcularDigitos'.

--- go/src/verificarCpf.go
+++ go/src/verificarCpf.go
@@ -6,18 +6,19 @@ import (
 	"strconv"
 )
 
-func VerificarCpf(cpf string) bool {
-	cpf = NewDigitos(cpf, 11).ReterNumeros()
-	if len(cpf) < 11 {
+func (c *Cpf) VerificarCpf() bool {
+	c.Cpf = NewDigitos(Digitos{*c, 11}).ReterNumeros()
+	if len(c.Cpf) < 11 {
 		log.Fatal("o CPF informado deve ter 11 dígitos")
 	}
 	var digitosRecebidos [2]int
-	primeiroDigito, _ := strconv.Atoi(cpf[9:10])
-	segundoDigito, _ := strconv.Atoi(cpf[10:11])
+	primeiroDigito, _ := strconv.Atoi(c.Cpf[9:10])
+	segundoDigito, _ := strconv.Atoi(c.Cpf[10:11])
 	digitosRecebidos[0] = primeiroDigito
 	digitosRecebidos[1] = segundoDigito
-	digitosCalculados := CalcularDigitosCpf(cpf)
-	cpfCompleto := fmt.Sprintf("%s.%s.%s-%d%d", cpf[0:3], cpf[3:6], cpf[6:9],
+	digitosCalculados := c.CalcularDigitosCpf()
+	cpfCompleto := fmt.Sprintf(
+		"%s.%s.%s-%d%d", c.Cpf[0:3], c.Cpf[3:6], c.Cpf[6:9],
 		digitosCalculados[0], digitosCalculados[1])
 	resultado, validez := "inválido", false
 	if digitosRecebidos == digitosCalculados {
