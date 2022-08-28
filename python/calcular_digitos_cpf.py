5b5696e1836c56921a63b933fd2e2b72a1187924
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-08-28 00:36:47 -0300

    Atualizando arquivos da pasta 'python': 'calcular_digitos_cpf.py', 'calcular_digitos_cpf.sh' e 'python/verificar_cpf.sh': adicionada impressão do CPF sem marcação, bastante útil para desenvolvedores.

--- python/calcular_digitos_cpf.py
+++ python/calcular_digitos_cpf.py
@@ -2,9 +2,9 @@ from cpf import Cpf
 
 def calcular_digitos(cpf):
     cpf = Cpf.reter_numeros(cpf, True)
-    
+
     assert len(str(cpf)) >= 9
-    
+
     cpf_informado = "{}.{}.{}".format(
         cpf[0:3], cpf[3:6], cpf[6:9]
     )
@@ -12,15 +12,19 @@ def calcular_digitos(cpf):
     digitos = Cpf.obter_dvs(cpf)
 
     resultado1 = "CPF informado: {}".format(cpf_informado)
-    
+
     resultado2 = "CPF completo:  {}".format(
         Cpf.marcar(str(cpf[:9]) + str(digitos[0]) + str(digitos[1]))
     )
-    
-    
+
+    resultado3 = "CPF completo:  {}{}{}".format(
+        cpf[:9], str(digitos[0]), str(digitos[1])
+    )
+
     print(resultado1)
     print(resultado2)
-    
+    print(resultado3)
+
     return (digitos[0], digitos[1])
 
 if __name__ == "__main__":
