7a2c187bbdbb712007f8103be0964fc1127a5a7b
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-10-15 17:09:58 -0300

    Removidos arquivos da pasta 'python': 'verificar_comprimento_cpf.py' e 'verificar_comprimento_cpf-test.py'.

--- python/calcular_digitos_cpf.sh
+++ python/calcular_digitos_cpf.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-toRed () { gawk -v text=$1 'BEGIN {
-    printf "%s", "\033[1;31m" text "\033[0m" }'
-}
-
 python3 /usr/local/lib/calcular-digitos-verificar-cpf/calcular_digitos_cpf.py $1;
 exit 0;
