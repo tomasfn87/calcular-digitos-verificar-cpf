59884a6a582e99fea9b34dc586628f93c6738287
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-10-16 01:35:16 -0300

    Alterados arquivos da pasta 'python': 'calcular_digitos_cpf.sh' e 'verificar_cpf.sh': os scripts agora retornam uma mensagem de erro caso não recebam argumento.

--- python/calcular_digitos_cpf.sh
+++ python/calcular_digitos_cpf.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-python3 /usr/local/lib/calcular-digitos-verificar-cpf/calcular_digitos_cpf.py $1;
-exit 0;
+toRed() { gawk -v text="$1" 'BEGIN {
+    printf "%s", "\033[1;31m" text "\033[0m" }'
+}
+
+if [ -z "$1" ];
+then
+    echo "$(toRed ERRO): informe um número de CPF.";
+    exit 1;
+else
+    python3 /usr/local/lib/calcular-digitos-verificar-cpf/calcular_digitos_cpf.py "$1";
+    exit 0;
+fi;
--- python/verificar_cpf.sh
+++ python/verificar_cpf.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-toGreen () { gawk -v text=$1 'BEGIN {
+
+toGreen () { gawk -v text="$1" 'BEGIN {
     printf "%s", "\033[1;32m" text "\033[0m" }'
 }
 
-toRed () { gawk -v text=$1 'BEGIN {
+toRed () { gawk -v text="$1" 'BEGIN {
     printf "%s", "\033[1;31m" text "\033[0m" }'
 }
 
-VALIDADE_CPF=$(python3 /usr/local/lib/calcular-digitos-verificar-cpf/verificar_cpf.py $1);
+VALIDADE_CPF=$(python3 /usr/local/lib/calcular-digitos-verificar-cpf/verificar_cpf.py "$1");
+
+if [ -z "$1" ];
+then
+    echo "$(toRed ERRO): informe um número de CPF.";
+    exit 2;
+fi;
 
 if [ $VALIDADE_CPF == 0 ];
 then
