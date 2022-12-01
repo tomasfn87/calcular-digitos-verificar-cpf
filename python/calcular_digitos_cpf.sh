72a43c18e733d7bf8ad6353d618addbb5997d6a1
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-12-01 19:38:09 -0300

    Alterada a formatação e alguns elementos da sintaxe dos arquivo da pasta 'python': 'calcular_digitos_cpf.sh' e 'verificar_cpf.sh'.

--- python/calcular_digitos_cpf.sh
+++ python/calcular_digitos_cpf.sh
@@ -4,18 +4,15 @@ toRed() { gawk -v text="$1" 'BEGIN {
     printf "%s", "\033[1;31m" text "\033[0m" }'
 }
 
+if [ -z "$1" ]; then
+    echo "`toRed ERRO`: informe um número de CPF.";
+    exit 1; fi;
 
-if [ -z "$1" ]
-then
-    echo "$(toRed ERRO): informe um número de CPF.";
-    exit 1;
-fi;
+CALCULO_CPF=`python3 /usr/local/lib/calcular-digitos-verificar-cpf/calcular_digitos_cpf.py "$1"`;
 
-CALCULO_CPF=$(python3 /usr/local/lib/calcular-digitos-verificar-cpf/calcular_digitos_cpf.py "$1");
-if [ "$CALCULO_CPF" == 1 ];
-then
-    echo "$(toRed ERRO): informe um número de CPF.";
-    exit 2;
-fi;
+if [ "$CALCULO_CPF" == 1 ]; then
+    echo "`toRed ERRO`: informe um número de CPF.";
+    exit 2; fi;
+
 echo "$CALCULO_CPF";
 exit 0;
--- python/verificar_cpf.sh
+++ python/verificar_cpf.sh
@@ -8,21 +8,18 @@ toRed () { gawk -v text="$1" 'BEGIN {
     printf "%s", "\033[1;31m" text "\033[0m" }'
 }
 
-if [ -z "$1" ]
-then
-    echo "$(toRed ERRO): informe um número de CPF.";
-    exit 3;
-fi;
+if [ -z "$1" ]; then
+    echo "`toRed ERRO`: informe um número de CPF.";
+    exit 3; fi;
 
-VALIDADE_CPF=$(python3 /usr/local/lib/calcular-digitos-verificar-cpf/verificar_cpf.py "$1");
-if [ "$VALIDADE_CPF" == 2 ];
-then
-    echo "$(toRed ERRO): informe um número de CPF.";
+VALIDADE_CPF=`python3 /usr/local/lib/calcular-digitos-verificar-cpf/verificar_cpf.py "$1"`;
+
+if [ "$VALIDADE_CPF" == 2 ]; then
+    echo "`toRed ERRO`: informe um número de CPF.";
     exit 2;
-elif [ "$VALIDADE_CPF" == 0 ];
-then
-    echo "CPF $(toGreen válido)";
-    exit 0;
-fi;
-echo "CPF $(toRed inválido)";
+elif [ "$VALIDADE_CPF" == 0 ]; then
+    echo "CPF `toGreen válido`";
+    exit 0; fi;
+
+echo "CPF `toRed inválido`";
 exit 1;
