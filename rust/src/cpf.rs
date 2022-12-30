cbc7ba90a1757d70a8d558fddd489d4cc8ef61a2
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-12-30 13:59:14 -0300

    Alterado arquivo 'rust/src/cpf.rs': alterada a ordem das funções, entendendo a função 'verificar' como a principal função e ponto de entrada do projeto.

--- rust/src/cpf.rs
+++ rust/src/cpf.rs
@@ -3,29 +3,30 @@ use pad::{PadStr, Alignment};
 use regex::Regex;
 use std::vec::IntoIter;
 
-pub fn reter_numeros(cpf: &str, n: usize) -> String {
+
+pub fn verificar(cpf: &str) -> bool {
     /*
-    Argumentos:
-    ----------
-    - cpf: ponteiro para string com números que serão isolados
-        de caracteres não numéricos; caso não existam caracteres
-        numéricos, será devolvida uma string vazia.
-    - n: inteiro que corresponde à quantidade de dígitos desejada, a
-        depender da operação em questão:
-        -  9 dígitos -> Cálculo dos dígitos verificadores de CPF;
-        - 11 dígitos -> Verificação do número de CPF.
+    Argumento:
+    ---------
+    - cpf: string (texto) com o número de CPF, com ou sem marcação; o
+        número será encarado com um CPF completo; seus dígitos finais
+        serão comparados com os dígitos calculados a partir dos 9
+        primeiros dígitos
     */
-    lazy_static! {
-        static ref REGEXP_NUMBERS: Regex = Regex::new(r"\d").unwrap();
+    let mut digitos_recebidos = vec![];
+    let mut c: u8 = 0;
+    for d in obter_digitos(cpf, 11) {
+        if c == 9 || c == 10 {
+            digitos_recebidos.extend(vec![d]);
+        }
+        c += 1
     }
-    lazy_static! {
-        static ref REGEXP_NON_NUMBERS: Regex = Regex::new(r"\D").unwrap();
+    let digitos_calculados: [i16; 2] = calcular_digitos(&cpf);
+    if digitos_recebidos[0] == digitos_calculados[0]
+        && digitos_recebidos[1] == digitos_calculados[1] {
+        return true
     }
-    if REGEXP_NUMBERS.is_match(cpf) {
-        let number_string = REGEXP_NON_NUMBERS.replace_all(cpf, "").to_string();
-        return number_string.pad(n, '0', Alignment::Right, true)
-    }
-    String::from("")
+    false
 }
 
 pub fn obter_digitos(cpf: &str, n: usize) -> IntoIter<i16> {
@@ -68,8 +69,32 @@ pub fn calcular_digitos(cpf: &str) -> [i16; 2] {
     }
     let digito_verificador_1 = calcular_digito_verificador(&digitos_cpf);
     digitos_cpf.extend(vec![digito_verificador_1]);
-    let digito_verificador_2 = calcular_digito_verificador(&digitos_cpf);
-    [ digito_verificador_1, digito_verificador_2 ]
+    [ digito_verificador_1, calcular_digito_verificador(&digitos_cpf) ]
+}
+
+pub fn reter_numeros(cpf: &str, n: usize) -> String {
+    /*
+    Argumentos:
+    ----------
+    - cpf: ponteiro para string com números que serão isolados
+        de caracteres não numéricos; caso não existam caracteres
+        numéricos, será devolvida uma string vazia.
+    - n: inteiro que corresponde à quantidade de dígitos desejada, a
+        depender da operação em questão:
+        -  9 dígitos -> Cálculo dos dígitos verificadores de CPF;
+        - 11 dígitos -> Verificação do número de CPF.
+    */
+    lazy_static! {
+        static ref REGEXP_NUMBERS: Regex = Regex::new(r"\d").unwrap();
+    }
+    lazy_static! {
+        static ref REGEXP_NON_NUMBERS: Regex = Regex::new(r"\D").unwrap();
+    }
+    if REGEXP_NUMBERS.is_match(cpf) {
+        let number_string = REGEXP_NON_NUMBERS.replace_all(cpf, "").to_string();
+        return number_string.pad(n, '0', Alignment::Right, true)
+    }
+    String::from("")
 }
 
 fn calcular_digito_verificador(digitos: &[i16]) -> i16 {
@@ -90,29 +115,4 @@ fn calcular_digito_verificador(digitos: &[i16]) -> i16 {
         return 0
     }
     11 - resto
-}
-
-pub fn verificar(cpf: &str) -> bool {
-    /*
-    Argumento:
-    ---------
-    - cpf: string (texto) com o número de CPF, com ou sem marcação; o
-        número será encarado com um CPF completo; seus dígitos finais
-        serão comparados com os dígitos calculados a partir dos 9
-        primeiros dígitos
-    */
-    let mut digitos_recebidos = vec![];
-    let mut c: u8 = 0;
-    for d in obter_digitos(cpf, 11) {
-        if c == 9 || c == 10 {
-            digitos_recebidos.extend(vec![d]);
-        }
-        c += 1
-    }
-    let digitos_calculados: [i16; 2] = calcular_digitos(&cpf);
-    if digitos_recebidos[0] == digitos_calculados[0]
-        && digitos_recebidos[1] == digitos_calculados[1] {
-        return true
-    }
-    false
 }
