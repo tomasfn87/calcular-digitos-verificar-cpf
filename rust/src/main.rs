06632e9231f1b17e7a5ac01b09b0e7dbaa8cb09b
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-12-31 04:42:53 -0300

    Alterado arquivo 'rust/src/main.rs': a impressão dos testes foi alinhada pelo caracter '='.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,34 +7,34 @@ fn main() {
     const B: &str = "111.444.777-35";
     const C: &str = "teste";
     const D: &str = "";
-    println!("A = \"{}\";    reter_numeros(A) = \"{}\", n = 11", A, cpf::reter_numeros(A, 11));
-    println!("B = \"{}\"; reter_numeros(B) = \"{}\",   n = 9", B, cpf::reter_numeros(B, 9));
-    println!("C = \"{}\";          reter_numeros(C) = \"{}\",            n = 11", C, cpf::reter_numeros(C, 11));
-    println!("D = \"{}\";               reter_numeros(D) = \"{}\",            n = 1", D, cpf::reter_numeros(D, 1));
+    println!("A = \"{}\";    reter_numeros(A, 11) = \"{}\"", A, cpf::reter_numeros(A, 11));
+    println!("B = \"{}\"; reter_numeros(B, 9)  = \"{}\"", B, cpf::reter_numeros(B, 9));
+    println!("C = \"{}\";          reter_numeros(C, 11) = \"{}\"", C, cpf::reter_numeros(C, 11));
+    println!("D = \"{}\";               reter_numeros(D, 1)  = \"{}\"", D, cpf::reter_numeros(D, 1));
     println!();
     let digitos_a1: IntoIter<i16> = cpf::obter_digitos(A, 11);
-    println!("CPF A = {A}");
-    println!("CPF A, 11 : digitos = {:?}", digitos_a1);
+    println!("                      CPF A = {A}");
+    println!("        CPF A, 11 : digitos = {:?}", digitos_a1);
     let digitos_a2: IntoIter<i16> = cpf::obter_digitos(A, 9);
-    println!("CPF A,  9 : digitos = {:?}", digitos_a2);
-    print!("Digitos verificadores CPF A = ");
+    println!("        CPF A,  9 : digitos = {:?}", digitos_a2);
+    print!("Digitos verificadores CPF A = [ ");
     for d in cpf::calcular_digitos(A) {
         print!("{} ", d)
     }
-    println!();
-    print!(          "Verificação CPF A = ");
+    println!("]");
+    print!("          Verificação CPF A = ");
     println!("{}", cpf::verificar(A));
     println!();
-    println!("CPF B = {B}");
+    println!("                      CPF B = {B}");
     let digitos_b1: IntoIter<i16> = cpf::obter_digitos(B, 11);
-    println!("CPF B, 11 : digitos = {:?}", digitos_b1);
+    println!("        CPF B, 11 : digitos = {:?}", digitos_b1);
     let digitos_b2: IntoIter<i16> = cpf::obter_digitos(B, 9);
-    println!("CPF B,  9 : digitos = {:?}", digitos_b2);
-    print!("Digitos verificadores CPF B = ");
+    println!("        CPF B,  9 : digitos = {:?}", digitos_b2);
+    print!("Digitos verificadores CPF B = [ ");
     for d in cpf::calcular_digitos(B) {
         print!("{} ", d)
     }
-    println!();
-    print!(          "Verificação CPF B = ");
+    println!("]");
+    print!("          Verificação CPF B = ");
     println!("{}", cpf::verificar(B));
 }
