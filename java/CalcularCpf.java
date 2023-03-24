c37d4f02281a0a65f5b1ec7a1a161b466e4e38cc
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-03-23 21:27:18 -0300

    Alterado arquivo 'java/CalcularCpf.java': o primeiro argumento de linha de comando é passado para o programa.

--- java/CalcularCpf.java
+++ java/CalcularCpf.java
@@ -1,9 +1,11 @@
 public class CalcularCpf {
     public static void main(String[] args) {
-        Cpf cpf = new Cpf("11144477711");
-        System.out.println(cpf.getCpf());
-        System.out.println(cpf.toString(".", "-"));
-        int[] dvs = cpf.calcularDigitos(); // 3, 5
+        if (args[0] != "") {
+            Cpf cpf = new Cpf(args[0]);
+            System.out.println(cpf.getCpf());
+            System.out.println(cpf.toString(".", "-"));
+            int[] dvs = cpf.calcularDigitos(); 
+        }
     }
 }
 
