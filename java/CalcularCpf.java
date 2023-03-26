93df2a730dbf6a5897883d31ec5b7f16984521e4
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-03-26 11:29:57 -0300

    Alterado arquivo 'java/CalcularCpf.java': adicionado teste do comprimento da variável 'args'.

--- java/CalcularCpf.java
+++ java/CalcularCpf.java
@@ -1,6 +1,6 @@
 public class CalcularCpf {
     public static void main(String[] args) {
-        if (args[0] != "") {
+        if (args.length > 0 && args[0] != "") {
             Cpf cpf = new Cpf(args[0]);
             System.out.println(cpf.getCpf());
             System.out.println(cpf.toString(' ', ' '));
