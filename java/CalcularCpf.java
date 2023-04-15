2ba64fc02fbae0265e83abce348a0291fcd3bc6f
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-04-15 09:05:43 -0300

    Alterados arquivos da pasta 'java': 'README.md', 'CalcularCpf.java' e 'Cpf.java': a impressão da entrada e da saída do programa foram alteradas para ilustrar melhor como funcionada o instanceamento do número de CPF (ver README.md).

--- java/CalcularCpf.java
+++ java/CalcularCpf.java
@@ -2,10 +2,12 @@ public class CalcularCpf {
     public static void main(String[] args) {
         if (args.length > 0 && args[0] != "") {
             Cpf cpf = new Cpf(args[0]);
-            System.out.println(cpf.getCpf());
-            System.out.println(cpf.toString(' ', ' '));
-            int[] dvs = cpf.calcularDigitos(); 
+            System.out.println("CPF recebido:");
+            System.out.printf("- %s%n", cpf.toString(' ', ' '));
+            System.out.printf("- %s%n", cpf.getCpf());
+            System.out.println("\nCálculo dígitos verificadores do CPF recebido:");
+            int[] dvs = cpf.calcularDigitos();
+            System.out.printf("%d, %d%n", dvs[0], dvs[1]);
         }
     }
 }
-
--- java/Cpf.java
+++ java/Cpf.java
@@ -2,20 +2,21 @@ import java.util.ArrayList;
 
 public class Cpf {
     private String cpf;
+
     public void setCpf(String cpf) {
-        this.cpf = cpf.replaceAll("\\D", "");
-    }
+        this.cpf = cpf.replaceAll("\\D", "");}
+
     public String getCpf() {
-        return this.cpf;
-    }
+        return this.cpf;}
+
     public Cpf(String cpf) {
         cpf = cpf.replaceAll("\\D", "");
         cpf = padLeft(cpf, 11, "0");
         if (cpf.length() > 0)
             this.cpf = cpf;
         else
-            this.cpf = "";
-    }
+            this.cpf = "";}
+
     public String toString(char marcador1, char marcador2) {
         if (marcador1 == ' ')
             marcador1 = '.';
@@ -25,9 +26,8 @@ public class Cpf {
             this.cpf.substring(0, 3), marcador1,
             this.cpf.substring(3, 6), marcador1,
             this.cpf.substring(6, 9), marcador2,
-            this.cpf.substring(9, 11)
-        );
-    }
+            this.cpf.substring(9, 11));}
+
     public int[] calcularDigitos() {
         int soma = 0;
         int resto = 0;
@@ -36,9 +36,8 @@ public class Cpf {
         String cpf = this.cpf.substring(0, 9);
         ArrayList<Integer> digitos = getNumericStringAsIntegerArray(cpf);
         for (int i=0; i<9; i++) {
-            soma = soma + (digitos.get(i) * multiplicador);
-            multiplicador--;
-        }
+            soma += digitos.get(i) * multiplicador;
+            multiplicador--;}
         resto = soma % 11;
         if (resto > 1)
             dvs[0] = 11 - resto;
@@ -46,38 +45,32 @@ public class Cpf {
         resto = 0;
         multiplicador = 11;
         digitos.add(dvs[0]);
-        System.out.println();
         for (int i=0; i<10; i++) {
-            soma += (digitos.get(i) * multiplicador);
-            multiplicador -= 1;
-        }
+            soma += digitos.get(i) * multiplicador;
+            multiplicador -= 1;}
         resto = soma % 11;
         if (resto > 1)
             dvs[1] = 11 - resto;
-        System.out.printf("dígitos verificadores: [%d, %d]%n", dvs[0], dvs[1]);
-        System.out.printf("CPF completo: %s%n",
-            String.format("%s.%s.%s-%d%d",
-            this.cpf.substring(0, 3),
-            this.cpf.substring(3, 6),
-            this.cpf.substring(6, 9),
-            dvs[0], dvs[1])
-        );
-        return dvs;
-    }
+        System.out.printf("- %s%n", String.format(
+            "%s.%s.%s-%d%d", this.cpf.substring(0, 3),
+            this.cpf.substring(3, 6), this.cpf.substring(6, 9),
+            dvs[0], dvs[1]));
+        System.out.printf("- %s%n", String.format(
+            "%s%d%d",
+            this.cpf.substring(0, 9), dvs[0], dvs[1]));
+        return dvs;}
+
     private String padLeft(String str, int n, String chr) {
         if (str == "" || chr.length() < 1 || n <= str.length())
             return str;
         if (chr.length() > 1)
             chr = chr.substring(0, 1);
         String pad = chr.repeat(n - str.length());
-        return String.format("%s%s", pad, str);
-    }
+        return String.format("%s%s", pad, str);}
+
     private ArrayList<Integer> getNumericStringAsIntegerArray(String num) {
         ArrayList<Integer> nums = new ArrayList<Integer>();
         for (int i=0; i<num.length(); i++) {
-            int n = Integer.parseInt(num.substring(i, i+1)); 
-            nums.add(n);
-        }
-        return nums;
-    }
-}
+            int n = Integer.parseInt(num.substring(i, i+1));
+            nums.add(n);}
+        return nums;}}
