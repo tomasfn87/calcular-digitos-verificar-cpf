ad168f7ae2284523eba448324c76de836d3ba7a6
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-06-24 00:07:14 -0300

    Alterados arquivos da pasta 'java': 1): removido arquivo 'CalcularCpf.java'; 2): alterado arquivo 'Cpf.java': o programa foi alterado para deixar de criar um objeto CPF e passar a receber uma string, que é testada por conteúdo numérico antes de proceder ao cálculo dígitos verificadores/verificação de CPF; 3): alterado arquivo 'README.md': o arquivo foi simplificado para corresponder à nova forma de funcionamento do programa.

--- java/Cpf.java
+++ java/Cpf.java
@@ -1,76 +1,96 @@
-import java.util.ArrayList;
-
 public class Cpf {
-    private String cpf;
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            if (args[0].equals("-c") || args[0].equals("--calcular")) {
+                if (isNumeric(args[1])) {
+                    int[] dvs = calcularDigitos(args[1], true);
+                    System.out.printf("{ %d, %d }%n", dvs[0], dvs[1]);}
+                else {
+                    System.out.println(
+                        "ERRO: o segundo argumento deve possuir ao menos um número para que seja realizado o cálculo dos dígitos verificadores do CPF.");}}
+            else if (args[0].equals("-v") || args[0].equals("--verificar")) {
+                if (isNumeric(args[1])) {
+                    boolean valid = verificar(args[1]);
+                    if (!valid) {
+                        System.out.printf("O CPF %s é inválido%n", marcar(args[1], ' ', ' '));}
+                    else {
+                        System.out.printf("O CPF %s é válido%n", marcar(args[1], ' ', ' '));}}
+                else {
+                    System.out.println(
+                        "ERRO: o segundo argumento deve possuir ao menos um número para que seja realizada a verificação do CPF.");}}}
+        else {
+            System.out.println(
+                "   - digite '-c' ou '--calcular' e um número de CPF para calcular os dígitos verificadores do CPF");
+            System.out.println(
+                "   - digite '-v' ou '--verificar' e um número de CPF para verificar o CPF");}}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf.replaceAll("\\D", "");}
+    public static boolean isNumeric(String str) {
+        if (str.length() < 1) {
+            return false;}
+        str = str.replaceAll("\\D", "");
+        if (str.length() > 0) {
+            return true;}
+        return false;}
 
-    public String getCpf() {
-        return this.cpf;}
-
-    public Cpf(String cpf) {
-        cpf = cpf.replaceAll("\\D", "");
-        cpf = padLeft(cpf, 11, "0");
-        if (cpf.length() > 0)
-            this.cpf = cpf;
-        else
-            this.cpf = "";}
-
-    public String toString(char marcador1, char marcador2) {
-        if (marcador1 == ' ')
-            marcador1 = '.';
-        if (marcador2 == ' ')
-            marcador2 = '-';
-        return String.format("%s%c%s%c%s%c%s",
-            this.cpf.substring(0, 3), marcador1,
-            this.cpf.substring(3, 6), marcador1,
-            this.cpf.substring(6, 9), marcador2,
-            this.cpf.substring(9, 11));}
-
-    public int[] calcularDigitos() {
+    public static int[] calcularDigitos(String cpf, boolean loud) {
+        String cpfRecebido = padLeftAndLimit(cpf.replaceAll("\\D", ""), 9, "0");
         int soma = 0;
         int resto = 0;
         int multiplicador = 10;
-        int[] dvs = {0, 0};
-        String cpf = this.cpf.substring(0, 9);
-        ArrayList<Integer> digitos = getNumericStringAsIntegerArray(cpf);
-        for (int i=0; i<9; i++) {
-            soma += digitos.get(i) * multiplicador;
+        int[] dvs = { 0, 0 };
+        for (int i = 0; i < 9; i++) {
+            soma += Integer.parseInt(cpfRecebido.substring(i, i+1)) * multiplicador;
             multiplicador--;}
         resto = soma % 11;
         if (resto > 1)
             dvs[0] = 11 - resto;
         soma = 0;
-        resto = 0;
         multiplicador = 11;
-        digitos.add(dvs[0]);
-        for (int i=0; i<10; i++) {
-            soma += digitos.get(i) * multiplicador;
-            multiplicador -= 1;}
+        for (int i = 0; i < 9; i++) {
+            soma += Integer.parseInt(cpfRecebido.substring(i, i+1)) * multiplicador;
+            multiplicador--;}
+        soma += dvs[0] * multiplicador;
         resto = soma % 11;
         if (resto > 1)
             dvs[1] = 11 - resto;
-        System.out.printf("- %s%n", String.format(
-            "%s.%s.%s-%d%d", this.cpf.substring(0, 3),
-            this.cpf.substring(3, 6), this.cpf.substring(6, 9),
-            dvs[0], dvs[1]));
-        System.out.printf("- %s%n", String.format(
-            "%s%d%d",
-            this.cpf.substring(0, 9), dvs[0], dvs[1]));
+        if (loud) {
+            System.out.printf("%s%n", String.format("%s.%s.%s-%d%d",
+                cpfRecebido.substring(0, 3), cpfRecebido.substring(3, 6),
+                cpfRecebido.substring(6, 9), dvs[0], dvs[1]));
+            System.out.printf("%s%n", String.format("%s%d%d",
+                cpfRecebido.substring(0, 9), dvs[0], dvs[1]));}
         return dvs;}
 
-    private String padLeft(String str, int n, String chr) {
-        if (str == "" || chr.length() < 1 || n <= str.length())
+    public static boolean verificar(String cpf) {
+        String cpfRecebido = padLeftAndLimit(cpf.replaceAll("\\D", ""), 11, "0");
+        int[] dvsRecebidos = {
+            Integer.parseInt(cpfRecebido.substring(9, 10)),
+            Integer.parseInt(cpfRecebido.substring(10, 11))};
+        int[] dvsCalculados = calcularDigitos(cpfRecebido, false);
+        if (dvsRecebidos[0] == dvsCalculados[0]
+            && dvsRecebidos[1] == dvsCalculados[1])
+            return true;
+        return false;}
+
+    public static String marcar(String cpf, char marcador1, char marcador2) {
+        cpf = cpf.replaceAll("\\D", "");
+        cpf = padLeftAndLimit(cpf, 11, "0");
+        if (marcador1 == ' ')
+            marcador1 = '.';
+        if (marcador2 == ' ')
+            marcador2 = '-';
+        return String.format("%s%c%s%c%s%c%s",
+            cpf.substring(0, 3), marcador1,
+            cpf.substring(3, 6), marcador1,
+            cpf.substring(6, 9), marcador2,
+            cpf.substring(9, 11));}
+
+    private static String padLeftAndLimit(String str, int n, String chr) {
+        if (str == "" || n <= str.length())
             return str;
         if (chr.length() > 1)
             chr = chr.substring(0, 1);
+        if (str.length() >= n)
+            return str.substring(0, n);
         String pad = chr.repeat(n - str.length());
-        return String.format("%s%s", pad, str);}
-
-    private ArrayList<Integer> getNumericStringAsIntegerArray(String num) {
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-        for (int i=0; i<num.length(); i++) {
-            int n = Integer.parseInt(num.substring(i, i+1));
-            nums.add(n);}
-        return nums;}}
+        return String.format("%s%s", pad, str);}}
--- java/README.md
+++ java/README.md
@@ -1,45 +1,25 @@
 # Calcular Dígitos CPF
 
-## Compilar
-
-
-```shell
-javac Cpf.java CalcularCpf.java
-```
-
-## Executar
-
-```shell
-java CalcularCpf 98765432111
-```
-
-<br>
-
 ## Como usar
 
-- Passar um número para cálculo dos dígitos verificadores;
-- Caso menos de 11 números forem recebidos, o número será preenchido com zeros à esquerda;
+### Compilar
 
-### _Lógica_
-
-- Na versão em `Java`, o programa segue uma lógica ligeiramente diferente:
-  - nas outras linguagens [desse projeto](https://github.com/tomasfn87/calcular-digitos-verificar-cpf), o cálculo usa apenas os primeiros 9 dígitos inseridos _(ou o CPF_ *__sem__* _os dígitos verificadores)_;
-  - em `Java`, ao usar o construtor da classe CPF estamos instanciando o CPF _(ou o CPF_ *__com__* _os dígitos verificadores)_.
-- Ao [usar o programa por linha de comando](#executar) o número recebido é considerado um *__CPF completo__*:
-  - se passarmos um número com 9 dígitos, os 2 últimos dígitos serão considerados os *__dígitos verificadores__* _(e não os 9 primeiros dígitos para então calcular os dígitos verificadores)_;
-  - na prática, se não soubermos os dígitos verificadores, precisamos passar dois números quaisquer como dígitos verificadores.
-
-### _Exemplos_
-
-```java
-Cpf cpf1 = new Cpf("1");           // IN "000.000.000-01", cálculo "000.000.000", OUT "000.000.000-00" 
-Cpf cpf2 = new Cpf("100");         // IN "000.000.001-00", cálculo "000.000.001", OUT "000.000.001-91"
-Cpf cpf3 = new Cpf("123456789");   // IN "001.234.567-89", cálculo "001.234.567", OUT "001.234.567-97" 
-Cpf cpf4 = new Cpf("12345678901"); // IN "123.456.789-01", cálculo "123.456.789", OUT "123.456.789-09"  
+```console
+javac Cpf.java
 ```
 
-### _Observações_
+### Executar
 
-- O cálculo é feito sobre os 9 primeiros dígitos entre os 11 que compõem um CPF;
-- Os 2 últimos dígitos são descartados, e, ao final do programa, o número correto é impresso;
-- Se o número informado e o número calculado forem iguais, o CPF é *__válido__*.
+#### Calcular dígitos verificadores CPF
+
+```console
+java Cpf -c 123.456.789
+java Cpf --calcular 987.654.321
+```
+
+#### Verificar CPF
+
+```console
+java Cpf -v 123.456.789-09
+java Cpf --verificar 987.654.321-09
+```
