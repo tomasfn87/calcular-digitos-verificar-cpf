b48c5ee262f3cf6918e133c33e7c0c45ff06f060
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-09-15 19:09:05 -0300

    Alterado arquivo 'perl/cpf.pl': adicionados métodos: 1): 'print_array': imprime um arranjo de elementos de forma simplificada; 2): 'calcular_digitos': recebe uma string e a verifica por conteúdo numérico - em caso positivo, será efetuado o cálculo de dígitos verificadores a partir dos 9 primeiros dígitos numéricos, e, caso menos de 9 números sejam recebidos, os números disponíveis serão complementados com zeros à esquerda - retorna um arranjo com 2 números (os dígitos verificadores); 3): 'verificar': recebe uma string que é igualmente verificada por conteúdo numérico, e, em caso positivo, é realizado o cálculo em cima dos 9 primeiros dígitos, isolados da string recebida e preenchida com zeros a esquerda para que tenha comprimento 11; os dígitos calculados são então comparados com os 2 últimos dígitos recebidos - se forem iguais, o CPF é válido, e, caso contrário, o CPF será dado como inválido.

--- perl/cpf.pl
+++ perl/cpf.pl
@@ -1,3 +1,14 @@
+sub print_array {
+    my (@array) = @_;
+    print "( ";
+    my $last_item_index = scalar(@array) - 1;
+    for my $i (0..$#array) {
+        if ($i == $last_item_index) {
+            print "$array[$i] )\n";
+            last;}
+        else {
+            print "$array[$i], ";}}}
+
 sub reter_numeros {
     my ($texto, $n) = @_;
     my $only_nums = "";
@@ -14,5 +25,54 @@ sub reter_numeros {
         $only_nums = "0" . $only_nums;}
     return $only_nums;}
 
-print(reter_numeros("test1", 1), "\n");
-print(reter_numeros("test1", 11), "\n");
+sub calcular_digitos {
+    my ($CPF) = @_;
+    my $cpf = reter_numeros($CPF, 9);
+    if (length($cpf) < 1) {
+        return undef;}
+    my @dvs = ( 0, 0 );
+    my ($soma, $multiplicador) = (0, 10);
+    for my $c (split //, $cpf) {
+        $soma += int($c) * $multiplicador;
+        $multiplicador -= 1;}
+    my $resto = $soma % 11;
+    if ($resto > 1) {
+        @dvs[0] = 11 - $resto;}
+    ($soma, $multiplicador) = (0, 11);
+    for my $c (split //, $cpf) {
+        $soma += int($c) * $multiplicador;
+        $multiplicador -= 1;}
+    $soma += @dvs[0] * $multiplicador;
+    $resto = $soma % 11;
+    if ($resto > 1) {
+        @dvs[1] = 11 - $resto;}
+    return @dvs;}
+
+sub verificar {
+    my ($CPF) = @_;
+    my $cpf = reter_numeros($CPF, 11);
+    if (length($cpf) < 1) {
+        return undef;}
+    my @dvs_recebidos = (
+        int(substr($cpf, 9, 1)),
+        int(substr($cpf, 10, 1)));
+    my @dvs_calculados = calcular_digitos(substr($cpf, 0, 9));
+    if (
+        @dvs_calculados[0] == @dvs_recebidos[0] and
+        @dvs_calculados[1] == @dvs_recebidos[1]) {
+        return 1;}
+    return 0;}
+
+print("reter_numeros(\"test1\", 1) = ", reter_numeros("test1", 1), "\n");
+print("reter_numeros(\"test1\", 11) = ", reter_numeros("test1", 11), "\n");
+print "\n";
+print("calcular_digitos(\"0\") = ");
+print_array(calcular_digitos("0"));
+print("calcular_digitos(\"192\") = ");
+print_array(calcular_digitos("192"));
+print("calcular_digitos(\"123\") = ");
+print_array(calcular_digitos("123"));
+print "\n";
+print("verificar(\"0\") = ", verificar("0"), "\n");
+print("verificar(\"19293\") = ", verificar("19293"), "\n");
+print("verificar(\"12360\") = ", verificar("12360"), "\n");
