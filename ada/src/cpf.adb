d78ab1ed7dff37691c2529d1bd67c3cc06bc4b66
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-09-28 22:41:01 -0300

    Alterado arquivo 'ada/src/cpf.adb': adicionada a função 'Verificar': recebe uma string e a testa por conteúdo numérico para realizar o cálculo dos dígitos verificadores de CPF excluindo os 2 últimos algarismos recebidos, e então comparar o resultado com os 2 últimos algarismos excluídos: se os 2 pares de algarimos forem formados por números iguais, o CPF é válido - caso contrário, o CPF é inválido.

--- ada/src/cpf.adb
+++ ada/src/cpf.adb
@@ -6,8 +6,8 @@ procedure Cpf is
 
    function Reter_Numeros (Texto : String; N : Natural) return String is
       Apenas_Numeros : Ada.Strings.Unbounded.Unbounded_String :=
-         Ada.Strings.Unbounded.Null_Unbounded_String;
-      Cont : Natural := 0;
+        Ada.Strings.Unbounded.Null_Unbounded_String;
+      Cont           : Natural                                := 0;
    begin
       for i in Texto'Range loop
          declare
@@ -34,36 +34,39 @@ procedure Cpf is
    end Reter_Numeros;
 
    function Calcular_Digitos (CPF : String) return DVs is
-      Digitos_Verificadores : DVs := (0, 0);
-      Num_Digitos_Base : constant Natural := 9;
-      Apenas_Numeros_CPF : String := Reter_Numeros (CPF, Num_Digitos_Base);
-      Soma : Natural := 0;
-      Multiplicador : Natural := Num_Digitos_Base + 1;
-      Resto : Natural := 0;
+      Digitos_Verificadores : DVs              := (0, 0);
+      Num_Digitos_Base      : constant Natural := 9;
+      Apenas_Numeros_CPF    : constant String  :=
+        Reter_Numeros (CPF, Num_Digitos_Base);
+      Soma                  : Natural          := 0;
+      Multiplicador         : Natural          := Num_Digitos_Base + 1;
+      Resto                 : Natural          := 0;
    begin
       for i in Apenas_Numeros_CPF'Range loop
          begin
-            Soma := Soma +
-               ((Character'Pos (Apenas_Numeros_CPF (i))
-               - Character'Pos ('0')) * Multiplicador);
+            Soma          :=
+              Soma +
+              ((Character'Pos (Apenas_Numeros_CPF (i)) - Character'Pos ('0')) *
+               Multiplicador);
             Multiplicador := Multiplicador - 1;
          end;
       end loop;
       Resto := Soma rem (Num_Digitos_Base + 2);
       if Resto > 1 then
-         Digitos_Verificadores(1) := (Num_Digitos_Base + 2) - Resto;
+         Digitos_Verificadores (1) := (Num_Digitos_Base + 2) - Resto;
       end if;
-      Soma := 0;
+      Soma          := 0;
       Multiplicador := Num_Digitos_Base + 2;
       for i in Apenas_Numeros_CPF'Range loop
          begin
-            Soma := Soma +
-               ((Character'Pos (Apenas_Numeros_CPF (i))
-               - Character'Pos ('0')) * Multiplicador);
+            Soma          :=
+              Soma +
+              ((Character'Pos (Apenas_Numeros_CPF (i)) - Character'Pos ('0')) *
+               Multiplicador);
             Multiplicador := Multiplicador - 1;
          end;
       end loop;
-      Soma := Soma + (Digitos_Verificadores (1) * Multiplicador);
+      Soma  := Soma + (Digitos_Verificadores (1) * Multiplicador);
       Resto := Soma rem (Num_Digitos_Base + 2);
       if Resto > 1 then
          Digitos_Verificadores (2) := (Num_Digitos_Base + 2) - Resto;
@@ -71,9 +74,27 @@ procedure Cpf is
       return Digitos_Verificadores;
    end Calcular_Digitos;
 
+   function Verificar (CPF : String) return Boolean is
+      Apenas_Numeros_CPF : constant String := Reter_Numeros (CPF, 11);
+      DVs_Recebidos      : constant DVs    :=
+        Calcular_Digitos (Apenas_Numeros_CPF (1 .. 9));
+      DVs_Calculados     : DVs             := (0, 0);
+   begin
+      DVs_Calculados (1) :=
+        Character'Pos (Apenas_Numeros_CPF (10)) - Character'Pos ('0');
+      DVs_Calculados (2) :=
+        Character'Pos (Apenas_Numeros_CPF (11)) - Character'Pos ('0');
+      if DVs_Calculados (1) = DVs_Recebidos (1)
+        and then DVs_Calculados (2) = DVs_Recebidos (2)
+      then
+         return True;
+      end if;
+      return False;
+   end Verificar;
+
    function Imprimir_DVs (Arr : DVs) return String is
       Result : Ada.Strings.Unbounded.Unbounded_String :=
-         Ada.Strings.Unbounded.To_Unbounded_String ("(");
+        Ada.Strings.Unbounded.To_Unbounded_String ("(");
    begin
       for i in Arr'Range loop
          Ada.Strings.Unbounded.Append (Result, Natural'Image (Arr (i)));
@@ -88,16 +109,28 @@ procedure Cpf is
    end Imprimir_DVs;
 
 begin
-   Ada.Text_IO.Put_Line ("Calcular_Digitos (""123.456.789"") = " &
+   Ada.Text_IO.Put_Line
+     ("Verificar (""123.456.789-09"") = " &
+      Boolean'Image (Verificar ("123.456.789-09")));
+   Ada.Text_IO.Put_Line
+     ("Verificar (""123.456.789-08"") = " &
+      Boolean'Image (Verificar ("123.456.789-08")));
+   Ada.Text_IO.Put_Line
+     ("Calcular_Digitos (""123.456.789"") = " &
       Imprimir_DVs (Calcular_Digitos ("123.456.789")));
-   Ada.Text_IO.Put_Line ("Calcular_Digitos (""123"") = " &
+   Ada.Text_IO.Put_Line
+     ("Calcular_Digitos (""123"") = " &
       Imprimir_DVs (Calcular_Digitos ("123")));
-   Ada.Text_IO.Put_Line ("Reter_Numeros (""test123"", 2)" &
-      " = """ & Reter_Numeros ("test123", 2) & """");
-   Ada.Text_IO.Put_Line ("Reter_Numeros (""test123"", 1)" &
-      " = """ & Reter_Numeros ("test123", 1) & """");
-   Ada.Text_IO.Put_Line ("Reter_Numeros (""test1"", 2)" &
-      " = """ & Reter_Numeros ("test1", 2) & """");
-   Ada.Text_IO.Put_Line ("Reter_Numeros (""test"", 1)" &
-      " = """ & Reter_Numeros ("test", 1) & """");
+   Ada.Text_IO.Put_Line
+     ("Reter_Numeros (""test123"", 2)" & " = """ &
+      Reter_Numeros ("test123", 2) & """");
+   Ada.Text_IO.Put_Line
+     ("Reter_Numeros (""test123"", 1)" & " = """ &
+      Reter_Numeros ("test123", 1) & """");
+   Ada.Text_IO.Put_Line
+     ("Reter_Numeros (""test1"", 2)" & " = """ & Reter_Numeros ("test1", 2) &
+      """");
+   Ada.Text_IO.Put_Line
+     ("Reter_Numeros (""test"", 1)" & " = """ & Reter_Numeros ("test", 1) &
+      """");
 end Cpf;
