9b96672c91826dace7965531a6feef75a6b455f3
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-12-15 21:16:41 -0300

    Alterado arquivo 'd/source/app.d': adicionada interface de linha de comando para verificar, calcular os dígitos ou formatar um número de CPF.

--- d/source/app.d
+++ d/source/app.d
@@ -1,15 +1,41 @@
 import cpf;
 import std.stdio;
+import std.format;
 
-void main() {
-	writeln("   reter_numeros(\"test\", 1) = \"", reter_numeros("test", 1), "\"");
-	writeln("reter_numeros(\"test123\", 0) = \"", reter_numeros("test123", 0), "\"");
-	writeln("reter_numeros(\"test123\", 1) = \"", reter_numeros("test123", 1), "\"");
-	writeln("reter_numeros(\"test123\", 2) = \"", reter_numeros("test123", 2), "\"");
-	writeln("reter_numeros(\"test123\", 4) = \"", reter_numeros("test123", 4), "\"");
-	writeln("  calcular_digitos(\"test0\") = ", calcular_digitos("test0"));
-	writeln("  calcular_digitos(\"test1\") = ", calcular_digitos("test1"));
-	writeln("calcular_digitos(\"test123\") = ", calcular_digitos("test123"));
-	writeln("     verificar(\"test19290\") = ", verificar("test19290"));
-	writeln("     verificar(\"test19291\") = ", verificar("test19291"));
-	writeln("     verificar(\"test12360\") = ", verificar("test12360"));}
+void main(string[] args) {
+	if (args.length < 3) {
+		print_help();
+		return;}
+
+	string operation = args[1], cpf = args[2];
+	if (!reter_numeros(cpf, 1).length) {
+		print_help();
+		return;}
+
+	if (operation == "-v" || operation == "--verificar") {
+		bool is_valid = verificar(cpf);
+		write(format("O CPF %s é ", formatar(cpf)));
+		if (is_valid) {
+			writeln("válido.");}
+		else {
+			writeln("inválido.");}} 
+	else 
+	if (operation == "-c" || operation == "--calcular") {
+		string CPF = reter_numeros(cpf, 9);
+		uint[2] digitos = calcular_digitos(CPF);
+		string cpf_completo = format("%s%d%d", CPF, digitos[0], digitos[1]);
+		writeln("CPF informado: ", formatar(format("%s%d%d", cpf, 0, 0))[0 .. 11]);
+		writeln("CPF completo:  ", formatar(cpf_completo));
+		writeln("               ", cpf_completo);
+		writeln(digitos);}
+	else 
+	if (operation == "-f" || operation == "--formatar") {
+		writeln(format("CPF formatado: %s", formatar(cpf)));}
+	else {
+        print_help();}}
+
+void print_help() {
+	writeln("Selecione uma das opções abaixo como primeiro argumento, e um número de CPF como segundo argumento:");
+	writeln(" * Digite '-v' para efetuar a verificação de um número de CPF;");
+	writeln(" * Digite '-c' para efetuar o cálculo dos dígitos verificadores de um número de CPF;");
+	writeln(" * Digite '-f' para efetuar a formatação de um número de CPF.");}
